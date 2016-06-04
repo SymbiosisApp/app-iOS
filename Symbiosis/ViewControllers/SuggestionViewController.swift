6ea730ee58ee8399b67c9d773c5c6efa6c1e424a
quentin.tshaimanga <q.tshaimanga@gmail.com>
2016-06-04 18:57:30 +0200

    button back

---
+++ Symbiosis/ViewControllers/SuggestionViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SuggestionViewController.swift
+//  symbiosis-ios-app
+//
+//  Created by Quentin Tshaimanga on 04/06/2016.
+//  Copyright © 2016 Etienne De Ladonchamps. All rights reserved.
+//
+
+import UIKit
+
+class SuggestionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("yolooooo")
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    
+    @IBAction func closeSuggestion(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+
+}
