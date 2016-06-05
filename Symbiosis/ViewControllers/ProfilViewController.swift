f65058aa969f130fe1936dc65d88021b457172ab
Etienne Dldc <etienne.dldc@outlook.fr>
2016-06-05 03:28:38 +0200

    Try Gifu : Gif trop lourd, ca crash :/

--- Symbiosis/ViewControllers/ProfilViewController.swift
+++ Symbiosis/ViewControllers/ProfilViewController.swift
@@ -8,30 +8,40 @@
 
 import Foundation
 import UIKit
+import Gifu
+
 
 class ProfilViewController: UIViewController {
     
     @IBOutlet weak var imageAnimTest: UIImageView!
+    @IBOutlet weak var gifuTest: UIImageView!
     
     
     override func viewDidLoad() {
         
-        self.imageAnimTest.contentMode =  UIViewContentMode.ScaleToFill
-        self.imageAnimTest.clipsToBounds = true
-        self.imageAnimTest.center = self.view.center
+//        self.imageAnimTest.contentMode =  UIViewContentMode.ScaleToFill
+//        self.imageAnimTest.clipsToBounds = true
+//        self.imageAnimTest.center = self.view.center
+//        
+//        //let priority = DISPATCH_QUEUE_PRIORITY_DEFAULT
+//        //dispatch_async(dispatch_get_global_queue(priority, 0)) {
+//            var images = [UIImage]()
+//            for i in 1...3 {
+//                print("Load image \(i)")
+//                let img = UIImage(named: "bienvenue-\(i)")!
+//                images.append(img)
+//            }
+//            self.imageAnimTest.animationImages = images
+//            
+//            self.imageAnimTest.startAnimating()
+//        //}
         
-        //let priority = DISPATCH_QUEUE_PRIORITY_DEFAULT
-        //dispatch_async(dispatch_get_global_queue(priority, 0)) {
-            var images = [UIImage]()
-            for i in 1...100 {
-                print("Load image \(i)")
-                let img = UIImage(named: "bienvenue-\(i)")!
-                images.append(img)
-            }
-            self.imageAnimTest.animationImages = images
-            
-            self.imageAnimTest.startAnimating()
-        //}
+        let animImg = AnimatableImageView(frame: gifuTest.frame)
+        
+        self.view.addSubview(animImg)
+        
+        animImg.animateWithImage(named: "bienvenue.gif")
+        animImg.startAnimating()
         
     }
     
